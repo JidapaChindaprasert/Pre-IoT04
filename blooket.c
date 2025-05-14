ed3737a2295b987379fdb17a2e747bcf4cbf653c
JidapaChindaprasert <164831889+JidapaChindaprasert@users.noreply.github.com>
2025-05-14 18:23:46 +0700

    not finish
    sorry Pern ป่วย งานอาจช้าหน่อย

---
+++ blooket.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+void twenty(){
+	int num1;
+	int num2 = 4;
+	scanf("%d", &num1);
+	printf("ans = %d",num1+num2);
+}
+void twentyone(){
+	char num1[] = "35";
+	int num2 = 34;
+	int ans = num1-num2;
+}
+void twentythree(){
+	printf(“แU๑ชoว์ตี้”);
+	printf(“\n๑ูLn๐เ๓้นหn้าrถllh่ Ln๐เ๓้นโช๐์ฅnIIn่\nLn๐เ๓้นโชw์เwื่๐nหn้าlวtี”);
+}
+
+void foutyone() {
+	int a = 5;
+	float b = 2.0;
+	printf("%f", a+b);
+	return ;
+}
+
+void fourtytwo() {
+	char c = 'A';
+	printf("%c", c);
+	return ;
+}
+
+void fourtythree() {
+	int x = 10;
+
+	if (x > 5) {
+		printf("Greater");
+	}else {
+		printf("Smaller");
+	}
+	return ;
+}
+
+void fourtyfour() {
+	int x = 5;
+	x++;
+	printf("%d", x);
+	return ;
+}
+
+void fourtyfive() {
+	printf("Hello, World!");
+	return ;
+}
+
+void fourtysix() {
+	printf("ก๋อมแก๋ม");
+	printf("คนแก่");
+	return ;
+}
+
+void fourtyseven() {
+	printf("ทุกคนมีแม่คนเดียว");
+	printf("\nโกโก้");
+	return ;
+}
+
+void fourtyeight() {
+	int x = 7.5;
+	printf("%d",x);
+	return ;
+}
+
+int main() 
+{
+	// twenty();
+	// twentyone();
+	// foutyone();
+	// fourtytwo();
+	// fourtythree();
+	// fourtyfour();
+	// fourtyfive();
+	// fourtysix();
+	// fourtyseven();
+	// fourtyeight();
+	return 0;
+}
